a2f49ad5995073033f1e44bc5561f0802bee5023
Elvira-94 <oredeine@tcd.ie>
2022-04-10 19:34:26 +0000

    Add initial readme class

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Readme:
+    """
+    A class to represent a readme entity
+
+    ...
+
+    Attributes
+    ----------
+    title : str
+        the title of the project
+
+    Methods
+    -------
+    add_section(section_type):
+        Adds a section of a given type to the readme object
+    """
+
+    def __init__(self, title):
+        self.title = title
+        self.section_classes = {
+            
+        }
+        self.sections = {}
+
+    def add_section(self, section_type):
+        """
+        Adds a section of a given type to the readme object
+
+            Parameters:
+                    section_type (str): String outlining the type of section to add. E.g. 'Intro'
+
+            Returns:
+                    NA
+        """
+        section = self.section_classes[section_type](self)
+        self.sections[section_type] = section
+
+
+
