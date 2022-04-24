4af773beac71c3dea71acc66843e0fbeccdf7633
Elvira-94 <oredeine@tcd.ie>
2022-04-24 09:22:34 +0000

    Clean pylint recommendations for readme.py

--- readme.py
+++ readme.py
@@ -1,5 +1,9 @@
-import sections
+"""
+This module contais the ReadMe class definition
+"""
+
 from colorama import Fore
+import sections
 
 class Readme:
     """
@@ -33,6 +37,11 @@ class Readme:
         self.intro_section = None
 
     def display_menu(self):
+        """
+        Displays the readme menu that is shown when a readme is currently
+        attached to the active session
+        """
+
         menu = {
             "prompt": "What would you like to do:",
             "type": "choice",
@@ -61,17 +70,15 @@ class Readme:
         menu.get('options').get(response).get('action')()
 
     def detach_from_session(self):
+        """
+        Detach the current readme object from the active session
+        Essentially returns the user to the main menu
+        """
         self.session.set_current_readme(None)
 
     def add_section(self):
         """
         Adds a section of a given type to the readme object
-
-            Parameters:
-                    section_type (str): String outlining the type of section to add. E.g. 'Intro'
-
-            Returns:
-                    NA
         """
 
         menu = {
@@ -95,9 +102,11 @@ class Readme:
 
         response = self.menu_handler.process_menu(menu)
 
-        # If a section has already been created before, call that section object rather than creating a new one
+        # If a section has already been created before, call that
+        # section object rather than creating a new one
         if self.sections.get(menu.get('options').get(response).get('prompt')):
-            self.sections.get(menu.get('options').get(response).get('prompt')).display_menu()
+            self.sections.get(menu.get('options').get(response).get('prompt'))\
+                .display_menu()
         else:
             section = menu.get('options').get(response).get('mapping')(self)
 
@@ -107,7 +116,15 @@ class Readme:
             ] = section
 
     def load_sections(self, worksheet):
-        
+        """
+        Given a google worksheet object, build section objects accordingly
+        and attach them to the current readme object
+
+            Parameters:
+            worksheet (gspread worksheet): The worksheet object returned from
+            Google sheets
+        """
+
         worksheet_data = worksheet.get_all_records()
 
         section_records = {}
@@ -118,19 +135,26 @@ class Readme:
             else:
                 section_records[row.get('Section Type')] = [row]
 
-        for key in section_records.keys():
-            section_class = self.section_types.get(key)
+        for item in section_records.items():
+            section_class = self.section_types.get(item[0])
 
             if section_class:
                 section_object = section_class(self)
-                section_object.load_section(section_records[key])
+                section_object.load_section(item[1])
 
-                self.sections[key] = section_object
+                self.sections[item[0]] = section_object
                 print(self.sections)
             else:
-                raise Exception(f"Unknown Class Found in README: {key}")
+                raise Exception(f"Unknown Class Found in README: {item[0]}")
 
     def preview_readme(self):
+        """
+        Calls self.output_raw and prints the contents of the output to
+        the teminal.
+
+        Waits for the user to provide any input before moving back to the
+        previous menu
+        """
         print(Fore.YELLOW + "\n\n\n|||||||||||||||||||||||")
         print("||                   ||")
         print("||       README      ||")
@@ -139,8 +163,10 @@ class Readme:
         print("|||||||||||||||||||||||\n\n\n" + Fore.WHITE)
 
         print(self.output_raw())
-        print(Fore.RED + "If the readme is longer than the terminal window,\n" + 
-            "please scroll up to view more content" + Fore.WHITE)
+        print(
+            Fore.RED + "If the readme is longer than the terminal window,\n"
+            + "please scroll up to view more content" + Fore.WHITE
+        )
         input(Fore.YELLOW + "Press enter to continue.." + Fore.WHITE)
 
     def output_raw(self):
@@ -155,13 +181,15 @@ class Readme:
         """
 
         output = ""
-        for section_type in self.sections:
-            output += self.sections[section_type].output_raw()
+        for section_type in self.sections.items():
+            output += section_type[1].output_raw()
 
         return output
 
     def output_to_file(self):
-
-        with open("Generated_README.md","w") as f:
-            f.write(self.output_raw())
-
+        """
+        Calls self.output_raw and wries the results to a README file locally.
+        This will only be useful if code is being ran loally and not in Heroku
+        """
+        with open("Generated_README.md", "w", encoding="utf-8") as file:
+            file.write(self.output_raw())
